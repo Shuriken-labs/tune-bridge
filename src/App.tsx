bb9e051c4122f5e7b5a53a6518c2a3f5022c6a13
victorfaruna <farunavictor043@gmail.com>
2025-05-20 16:18:02 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import './App.css'
-import Navbar from './component/header'
-import Hero from './component/hero'
-import heroimg from './assets/Property 1=Desktop - 2.png'
-
+import "./App.css";
+import Navbar from "./component/header";
+import Hero from "./component/hero";
+import heroimg from "./assets/Property 1=Desktop - 2.png";
 
 function App() {
-   return (
-    <div className="flex flex-col text-amber-50"
-    style={{
-        backgroundImage: `url(${heroimg})`,
-      }}>
-      <Navbar/>
-      <Hero/>
-    </div>
-  )
+    return (
+        <div
+            className="flex flex-col text-amber-50"
+            style={{
+                backgroundImage: `url(${heroimg})`,
+            }}
+        >
+            <Navbar />
+            <Hero />
+        </div>
+    );
 }
 
-export default App
+export default App;
